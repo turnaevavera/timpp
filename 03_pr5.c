961fe5fcdf7f9867aab6be67a8a9c253b3688fd6
turnaevavera <47891348+turnaevavera@users.noreply.github.com>
2019-04-26 17:04:45 +0700

    Update 03_pr5.c

--- 03_pr5.c
+++ 03_pr5.c
@@ -1,245 +1,253 @@
-#include<stdio.h> 
-#include<stdlib.h> 
-#include<stdbool.h>
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <stdbool.h>
 #include <string.h>
 
-typedef struct node
+typedef struct strr
 {
-  int data;
-  struct node * left;
-  struct node * right;
-  struct node * parent;
-} node;
- 
-typedef struct tree
-{
-  struct node * root;   
-  int count;            
-} tree;
+    int data;
+    struct strr * left;
+  struct strr * right;
+  struct strr * parent;
+}
+strr;
 
-void init(tree * t)
+typedef struct derev
 {
-    struct tree * new_t = malloc(sizeof new_t);
-    new_t->root = NULL;
-    new_t->count = 0;
-    t = new_t;
+    struct strr * root;
+  int count;
+}
+derev;
+
+void init(derev* t)
+{
+    struct derev * new_derev = malloc(sizeof new_derev);
+	new_derev->root = NULL;
+    new_derev->count = 0;
+    t = new_derev;
 }
 
-int find(tree* t, int value, node* n)
+int find(derev* t, int value, strr* n)
 {
-  struct node * b;
-  b = t->root;
-
-  if (t->root == NULL){
+  struct strr * b;
+b = t->root;
+  if (t->root == NULL)
+  {
     return 1;
   }
-
-
-  while (1){
-    if (b == NULL) {
+  while (1)
+  {
+    if (b == NULL)
+	{
       return 1;
-    } else if (b->data == value){
-
+    }
+	else if (b->data == value)
+	{
       n->data = b->data;
       n->left = b->left;
       n->right = b->right;
       n->parent = b->parent;
-
       return 0;
-    } else if (value > b->data){
+    } 
+	else if (value > b->data)
+	{
       b = b->right;
-    } else {
+    } 
+	else 
+	{
       b = b->left;
     }
   }
-
   return 1;
 }
 
-int insert(tree* t, int value)
+int insert(derev* t, int value)
 {
-  struct node * n, ** bb, * last_n = NULL;
-
-  struct node * e_n;
-  e_n = malloc(sizeof * e_n);
-
-  int err = find(t, value, e_n);
-  if (err == 0){
+  struct strr * n, ** b, * last_strr = NULL;
+struct strr * en;
+en = malloc(sizeof * en);
+    
+int error = find(t, value, en);
+  if (error == 0)
+  {
     return 1;
   }
-
-  bb = &t->root;
+  b = &t->root;
   n = t->root;
-
-  while (1){
-    if (n == NULL) {
-      n = *bb = malloc(sizeof * n);
-      if (n != NULL){
+  while (1)
+  {
+    if (n == NULL) 
+	{
+      n = * b = malloc(sizeof * n);
+      if (n != NULL)
+	  {
         n->data = value;
         n->left = NULL;
         n->right = NULL;
-        n->parent = last_n;
-
+        n->parent = last_strr;
         t->count++;
         return 0;
-      } else {
-       
+      } 
+	  else 
+	  {
         return 2; 
       }
     }
-    last_n = n;
-    
-    if (value > n->data){
-      bb = &n->right;
+    last_strr = n;  
+    if (value > n->data)
+	{
+      b = &n->right;
       n = n->right;
-    } else {
-      bb = &n->left;
+    }
+	else
+	{
+      b = &n->left;
       n = n->left;
     }
   }
   return 0;
 }
 
-int deepness(struct node * n, int deep){
-  if (n == NULL){
+int deepness(struct strr * n, int deep)
+{
+  if (n == NULL)
+  {
     return deep;
   }
-  int d1 = deepness(n->left, deep + 1);
-  int d2 = deepness(n->right, deep + 1);
-
-  return (d1 > d2) ? d1 : d2;
+    
+int d1 = deepness(n->left, deep + 1);
+int d2 = deepness(n->right, deep + 1);
+return (d1 > d2) ? d1 : d2;
 }
 
-void printNode(struct node * n, int flow, int deep, int perv){
-  //printf("flow: %d, deep: %d\n", flow, deep);
-
-  if (flow == deep){
-      if (perv > 0){
+void printstrr(struct strr * n, int flow, int deep, int perv)
+{
+  if (flow == deep)
+  {
+      if (perv > 0)
+	  {
         printf(" ");
-      }
-      
-    if (n == NULL){
+      } 
+    if (n == NULL)
+	{
       printf("_");
-    } else{
+    }
+	else
+	{
       printf("%d", n->data);
     } 
-  } else if (n != NULL){
-    printNode(n->left, flow + 1, deep, perv);
-    printNode(n->right, flow + 1, deep, perv + 1);
-  } else {
-    printNode(n, flow + 1, deep, perv);
-    printNode(n, flow + 1, deep, perv + 1);
+  } 
+  else if (n != NULL)
+  {
+    printstrr(n->left, flow + 1, deep, perv);
+printstrr(n->right, flow + 1, deep, perv + 1);
   }
+  else
+	{
+    printstrr(n, flow + 1, deep, perv);
+	printstrr(n, flow + 1, deep, perv + 1);
+	}
 }
 
-void print(struct node * n)
+void print(struct strr * n)
 {
   int m = deepness(n, 0);
-  for (int i = 1; i <= m; i++){
-    printNode(n, 1, i, 0);
-    printf("\n");
+  for (int i = 1; i <= m; i++)
+  {
+  printstrr(n, 1, i, 0);
+  printf("\n");
   }
-}   
-
-void printTree(struct tree * t)
+}
+   
+void printderev(struct derev * t)
 {
     print(t->root);
 }
 
-void printEx1(struct node * n)
+void print_obh1(struct strr * n)
 {
   int m = deepness(n, 0);
-  int xex = 0;
-  for (int i = 1; i <= m; i++){
-    if (xex > 0){
+int xex = 0;
+  for (int i = 1; i <= m; i++)
+  {
+    if (xex > 0)
+	{
         printf(" ");
-    } else {
+    } 
+	else 
+	{
         xex++;
     }
-    printNode(n, 1, i, 0);
+    printstrr(n, 1, i, 0);
   }
 }   
 
-void printEx2(struct tree * t)
+void print_obh2(struct derev * t)
 {
-    node * a[15];
-    int a_i = 0;
-    
-    node * write[15];
-    int w_i = 0;
-    
-    node * n = t->root;
-    
-    while (w_i < t->count){ 
-        while (n != NULL){
-            // printf("flow n: %d\n", n->data);
-            w_i++;
-            if (n->right != NULL){
-                a_i++;
-                a[a_i] = n->right;
+    strr* a[15];
+int ab = 0;
+strr* write[15];
+int wb = 0;
+strr* n = t->root;
+    while (wb<t->count)
+	{ 
+        while (n != NULL)
+		{
+            wb++;
+            if (n->right != NULL)
+			{
+                ab++;
+                a[ab] = n->right;
             }
-            write[w_i] = n;
+            write[wb] = n;
             n = n->left;
-            
-            // printf("a:");
-            // for (int i = 1; i <= a_i; i++){
-            //     printf(" %d", a[i]->data);
-            // }
-            // printf("\n");
-            // printf("w_i: %d\n", w_i);  
         }
-        n = a[a_i];
-        a_i -= 1;
-        
-    }
-    
+        n = a[ab];
+        ab -= 1;    
+    }  
     int xex = 0;
-    for (int i = 1; i <= w_i; i++){
-        if (xex > 0){
+    
+    for (int i = 1; i <= wb; i++)
+	{
+        if (xex > 0)
+		{
             printf(" ");
         } else {
             xex++;
         }
         printf("%d", write[i]->data);
-    }
-    
+    } 
   printf("\n");
 }   
 
-void printEx3(struct node * n, int xex)
+void print_obh3(struct strr * n, int xex)
 {
-    if (n->left != NULL){
-        printEx3(n->left, xex + 1);
+    if (n->left != NULL)
+	{
+        print_obh3(n->left, xex + 1);
     } 
-    if (n->right != NULL){
-        printEx3(n->right, xex + 1);
+    if (n->right != NULL)
+	{
+        print_obh3(n->right, xex + 1);
     }
     printf("%d", n->data);
-    if (xex > 0){
+    if (xex > 0)
+	{
         printf(" ");
-    }
-    
+    }   
 }   
 
-int main(){
-
-  struct tree * t = malloc(sizeof t);
-  init(t);
-
-
-
-  for (int i = 0; i < 7; i++){
+int main()
+{
+  struct derev * t = malloc(sizeof t);
+init(t);
+  for (int i = 0; i< 7; i++)
+  {
     int a;
-    scanf("%d", &a);
-    insert(t, a);
+scanf("%d", &a);
+insert(t, a);
   }
-  
-    
-  //printEx1(t->root);
-  //printf("\n");
-  //printEx2(t);
-  printEx3(t->root, 0);
-  printf("\n");
-
+  print_obh3(t->root, 0);
+printf("\n");
   return 0;
 }
