1cb0e98f0ca27fc376cb81590654e927a0822265
turnaevavera <47891348+turnaevavera@users.noreply.github.com>
2019-06-01 01:53:41 +0700

    метод Шелла

--- 02_laba.c
+++ 02_laba.c
@@ -1,15 +1,8 @@
 #include <stdio.h>
-
 #include <stdlib.h>
-
 #include<time.h>
-
-
-
 int sort(int *chisl, int size)
-
 {
-
 	int i, j,temp=0;
 
 	int sm;
@@ -43,85 +36,48 @@ int sort(int *chisl, int size)
 				    temp++;
 
 					break;
-
 				}
 
 				temp++;
-
 			}
 
 			chisl[j] = tmp;
-
 		}
-
 	}
 
 	return temp;	
-
-		
-
-		
 
 }
 
 int main()
 
 {
-
     srand(time(0));
-
   clock_t start,stop;
-
 unsigned long t;
-
     double rez,sr_rez = 0;
-
 int n[15] = {1,2,3,4,5,10,15,20,25,30,50,75,100,250,500};
-
 for (int f = 0 ; f <15;f++)
-
 {
-
 int *a;
-
 a = (int*)malloc(n[f] * sizeof(int));
-
 start = clock();
-
 for (int j = 0;j <1000;j++)
-
 {
-
 for (int i = 0;i < n[f]; i ++)
-
 {
-
 a[i] = rand()%  10000 - 8419;
-
 } 
-
 rez = sort(a,n[f]);
-
 sr_rez +=rez;
-
 }
-
 stop = clock();
-
 printf("%d\n %f \n",n[f],sr_rez/1000);
-
 sr_rez = 0;
-
 rez = 0;
-
 double clock_rez = (stop - start)/(double)CLOCKS_PER_SEC;
-
 printf("%f \n",clock_rez*1000000);
-
 start ,stop = 0;
-
 clock_rez = 0;
-
 }
-
 }
